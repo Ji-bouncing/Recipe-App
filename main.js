aa5c8472da2d825e5dc935198162dd8204800583
abdullah100dev2002@gmail.com <abdullah100dev2002@gmail.com>
2024-01-25 09:50:25 +0100

    Recipe App Project

---
+++ main.js
@@ -0,0 +1,30 @@
+
+document.querySelector('#getDish').addEventListener('click',fetchDish)
+
+function fetchDish(){
+  const dish = document.querySelector('#inputValue').value.toLowerCase()
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${dish}`
+  fetch(url)
+  .then(res => res.json())
+  .then( data => {
+    console.log(data.meals)
+    data.meals.forEach( obj => {
+      document.querySelector('img').src = obj.strMealThumb
+      document.querySelector('#DishName').innerText = obj.strMeal
+      document.querySelector('h3').innerText = obj.strArea
+      document.querySelector('section').style.display = 'block'
+      document.querySelector('img').style.display = 'block'
+      //create a list of ingredients
+          const li = document.createElement('li')
+          li.textContent = obj.strIngredient1
+          document.querySelector('ul').appendChild(li)
+
+     document.querySelector('#putRecipe').innerText = obj.strInstructions
+
+    })
+
+  })
+  .catch( err => {
+    console.log(`Error ${err}`)
+  });
+}
